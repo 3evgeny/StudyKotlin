61238f153ea5b0388960cb5e4fe90c7b8417037e
melself <maskot2002@yandex.ru>
2022-10-25 18:59:38 +0300

    PR 2 - Task 1

---
+++ src/main/kotlin/PR2/Task1.kt
@@ -0,0 +1,13 @@
+package PR2
+
+//Напишите функцию countVowels подсчитывающую число
+//гласных английских букв (а, е, i, о, и) в строке, у которая
+//передаётся в виде параметра
+
+fun main(args: Array<String>){
+    println(countVowels("asdfghjkl"))
+}
+
+fun countVowels(src: String): Int{
+    return src.lowercase().count { char -> char in "aeiou" }
+}
--- src/main/kotlin/PR2/pr2.kt
+++
@@ -1,5 +0,0 @@
-package PR2
-
-fun main(args: Array<String>){
-
-}
