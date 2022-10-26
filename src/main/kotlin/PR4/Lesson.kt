965d15bc75b11df11f6b7c513414717dafbc289e
melself <maskot2002@yandex.ru>
2022-10-26 14:23:10 +0300

    Done PR4

---
+++ src/main/kotlin/PR4/Lesson.kt
@@ -0,0 +1,20 @@
+package PR4
+
+import kotlin.system.measureNanoTime
+
+fun main(){
+    val list = ArrayList<Long>()
+    repeat(10_000_00){
+        list.add(it.toLong())
+    }
+
+    val set = list.toSet()
+    val listTime = measureNanoTime {
+        val a = list.contains(5_000_000)
+    }
+    val setTime = measureNanoTime {
+        val a = set.contains(5_000_000)
+    }
+
+    println("List: ${listTime}ns, Set: ${setTime}ns")
+}
