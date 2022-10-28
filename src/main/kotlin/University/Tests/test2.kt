282943eb259aeb4e95d71ea2c59da41d5a0cf4b5
melself <maskot2002@yandex.ru>
2022-10-28 18:11:48 +0300

    SoloLearn

---
+++ src/main/kotlin/University/Tests/test2.kt
@@ -0,0 +1,40 @@
+//package University.Tests
+//
+//fun main(){
+//    printMessage("A")
+//    val sum: (Int, Int) -> Int = { a, b -> a + b }
+//    val sum2 = { a: Int, b: Int -> a + b }
+//
+//    println(sum3(3,4F)::class)
+//    println(square(2.0))
+//    //val result: Double = div(3, 2)
+//    //println(result)
+//    println(sum3)
+//    val result: Double = div(3, 2)
+//
+//}
+//
+//fun sum3(a: Int, b: Float) = a + b
+//
+//fun printMessage(m: String) =  println(m)
+//
+//fun rollDice(i: Int): Long = Math.round(Math.random() * i)
+//
+//fun sum(a: Int, b: Int): Int {
+//    return a + b
+//}
+//
+//fun div(a: Int, b: Int): Double = a / b
+//
+//fun sum2(a: Int, b: Int) = a + b
+//fun square(l: Double) = Math.pow(l, 2.0)
+//val sum3 = { a: Int, b: Int -> a + b }
+//
+//
+//
+//
+//
+//fun su3(a: Int, b: Int): Int {
+//    return a + b
+//}
+//
---
+++ src/main/kotlin/University/Tests/test3.kt
@@ -0,0 +1,14 @@
+package University.Tests
+
+class Device {
+    val run: Boolean = true
+    var stop: Boolean = false
+}
+
+fun main() {
+    var dev = Any()
+    // ?
+    dev = Any()
+    dev = Device()
+
+}
