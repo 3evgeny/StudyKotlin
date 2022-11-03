c209ff72b336ea01f564853054225cc2f4235a20
melself <maskot2002@yandex.ru>
2022-11-03 09:40:39 +0300

    SoloLearn

--- src/main/kotlin/SoloLearn/Tasks/main.kt
+++ src/main/kotlin/SoloLearn/Tasks/main.kt
@@ -1,17 +1,17 @@
 package SoloLearn.Tasks
 
-abstract class Text(val pg:Int){
+abstract class Text(){
     abstract fun letters(): Int
      fun isTextTooBig(): Boolean {
-         return pg>100500
+         return letters()>=100500
      }
 }
 
-internal class Book (val pages: Int) : Text(pages) {
+internal class Book (val pages: Int) : Text() {
     override fun letters() = pages * 1000
 }
 
 fun main() {
-    println(Book(100520).isTextTooBig())
+    println(Book(1001).isTextTooBig())
     println(Book(1).isTextTooBig())
 }
